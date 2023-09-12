6040fa8aa851730942277e730670540dc309bff8
EUROPE\Aly_Magdy <aly_magdy@dell.com>
2023-09-12 23:11:37 +0300

    Create file

--- main.py
+++ main.py
@@ -5,4 +5,12 @@ def add(a, b):
 num=add(5,5)
 print(f"Number equals to ========= {num}\n")
 
+# Open a file named "output.log" in write mode ('w')
+with open('output.log', 'w') as file:
+    # Write "Hello, World!" to the file
+    file.write('Hello, World!')
+
+# Print "Hello, World!" to the console
+print('Hello, World!')
+
 #Testing new commit jenkins
