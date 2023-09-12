41936bebc617308413a007d1a2e4ebdfd4a5d1f2
EUROPE\Aly_Magdy <aly_magdy@dell.com>
2023-09-12 23:12:31 +0300

    Create file

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ print(f"Number equals to ========= {num}\n")
 # Open a file named "output.log" in write mode ('w')
 with open('output.log', 'w') as file:
     # Write "Hello, World!" to the file
-    file.write('Hello, World!')
+    file.write(f'Hello, World! , the function returns {num}')
 
 # Print "Hello, World!" to the console
 print('Hello, World!')
